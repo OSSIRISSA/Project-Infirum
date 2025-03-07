0a5688fa64b0b7eb0d48a08ca9be5d15ca815c9d
Mess0r <pipcovich003@gmail.com>
2025-03-07 18:34:39 +0100

    V.alpha.0.1.3
     - Better, Faster Inventory
     - Removed Cryptor Component
     - All inventory logic directly in slots and WBP

---
+++ Source/Project_Infirum/InventoryGrid.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Widget.h"
+#include "InventoryGrid.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_INFIRUM_API UInventoryGrid : public UWidget
+{
+	GENERATED_BODY()
+	
+};
